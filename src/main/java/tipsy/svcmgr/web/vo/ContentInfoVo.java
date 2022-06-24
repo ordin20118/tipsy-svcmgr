287b1f19bef66e6de2ec8fd19dec6292474ecb72
GwangA <ordin20118@gmail.com>
2022-06-25 03:06:53 +0900

    Change content type code. Modify equip select sql.

--- src/main/java/tipsy/svcmgr/web/vo/ContentInfoVo.java
+++ src/main/java/tipsy/svcmgr/web/vo/ContentInfoVo.java
@@ -3,8 +3,9 @@ package tipsy.svcmgr.web.vo;
 public class ContentInfoVo {
 	
 	public static final int CONTENT_TYPE_LIQUOR 			= 100;	// 술
-	public static final int CONTENT_TYPE_MATERIAL 			= 110;	// 술 재료 (라임, 소금, 시나몬 등...)
-	public static final int CONTENT_TYPE_TOOL				= 120;	// 술 도구
+	public static final int CONTENT_TYPE_COCKTAIL			= 200;	// 칵테일
+	public static final int CONTENT_TYPE_MATERIAL 			= 300;	// 술 재료 (라임, 소금, 시나몬 등...)
+	public static final int CONTENT_TYPE_TOOL				= 400;	// 술 도구
 	public static final int CONTENT_TYPE_PAIRING 			= 300;	// 페어링 음식
 	public static final int CONTENT_TYPE_INT_REVIEW 		= 400;	// 내부 평가 - 코드는 좀 더 고려 필요
 	public static final int CONTENT_TYPE_EXT_REVIEW			= 500;	// 외부 리뷰
